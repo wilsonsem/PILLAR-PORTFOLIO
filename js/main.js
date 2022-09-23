c0cf9d862733e65360d598e2dab3fb06dec158b7
Semmwa <bakshaksemmwa@gmail.com>
2022-09-23 23:02:40 +0100

    landing page for presentation

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".auto-type", {
+    strings: ["Music in my soul", "Music is life", "Music heals"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50
+    // backDelay: 2000
+    });
